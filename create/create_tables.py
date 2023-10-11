2db4fa53b172abaf43e1e18ab79dd5b520117811
Sindhu Murthy <sindhumurthy97@gmail.com>
2023-10-11 14:47:27 -0400

    Automatic table with pk, fk

--- create/create_tables.py
+++ create/create_tables.py
@@ -1,6 +1,8 @@
 import psycopg2
 import sshtunnel
+import glob, os
 import csv
+import numpy as np
 import pwinput
 from psycopg2 import sql
 
@@ -20,14 +22,32 @@ cursor = conn.cursor()
 schema_name = 'public'  # Change this if your tables are in a different schema
 print('Connection successful. \n')
 
-def get_column_names(col1, col2):
-    col1_list = [item.strip() for item in col1.split(',')]
-    col2_list = [item.strip() for item in col2.split(',')]
+def get_csv_fname(loc):
+    os.chdir(loc)
+    fnameLs = glob.glob("*.csv")
+    # fnameLs = [loc+i for i in fnameLs]
+    return fnameLs
+
+def get_table_info(loc, fname):
+    with open(loc+fname, mode ='r') as file:
+        csvFile = csv.reader(file)
+        rows = []
+        for row in csvFile:
+            rows.append(row)
+        temp = np.array(rows).T
+        fk = temp[0][(np.where(temp[-1] != ''))]
+        fk_ref = temp[-2][(np.where(temp[-1] != ''))]
+        fk_tab = temp[-1][(np.where(temp[-1] != ''))]
+        return fname.split('.csv')[0], temp[0], temp[1], fk, fk_ref, fk_tab   ### fk, fk_tab are returned as lists
+    
+def get_column_names(col1_list, col2_list, fk_name, fk_ref, parent_table):
     combined_list = []
     for item1, item2 in zip(col1_list, col2_list):
         combined_list.append(f'{item1} {item2}')
-    return ', '.join(combined_list)
-
+    table_columns = ', '.join(combined_list)
+    if fk_name.size != 0:
+        table_columns += f', CONSTRAINT {fk_ref[0]} FOREIGN KEY({fk_name[0]}) REFERENCES {parent_table[0]}({fk_name[0]})'
+    return table_columns
 
 def create_table(schema_name, table_name, table_columns):
     # Check if the table exists
@@ -48,15 +68,20 @@ def create_table(schema_name, table_name, table_columns):
     cursor.execute(f"GRANT SELECT ON {table_name} TO viewer;")
     print(f"Table '{table_name}' access granted to user ogp_user, lv_user.\n")
 
-csv_file_path = '../dbase_info/postgresTables.csv'
 
-# Define the table name and schema
-with open(csv_file_path, 'r') as file:
-    csv_reader = csv.reader(file)
-    for row in csv_reader:
-        table_name, table_write, table_read, table_header, dat_type = row
-        table_columns = get_column_names(table_header, dat_type)
-        create_table(schema_name, table_name, table_columns)
+######################################################################
+
+loc = '../dbase_info/'
+# fname_list = get_csv_fname(loc)
+fname_list = ['module_assembly.csv', 'proto_assembly.csv','baseplate.csv', 'sensor.csv', 'hexaboard.csv', 'bp_inspect.csv', 'hxb_inspect.csv', 'proto_inspect.csv','module_inspect.csv', 'module_test.csv']
+
+## Define the table name and schema
+for fname in fname_list:
+    print(f'Getting info from {fname} ...')
+    table_name, table_header, dat_type, fk_name, fk_ref, parent_table = get_table_info(loc, fname)
+    table_columns = get_column_names(table_header, dat_type, fk_name, fk_ref, parent_table)
+    create_table(schema_name, table_name, table_columns)
+
 
 # for table_name in table_names_list:
 #     give_user_access(table_name)
@@ -65,5 +90,3 @@ conn.commit()
 # Close the connection
 cursor.close()
 conn.close()
-
-
