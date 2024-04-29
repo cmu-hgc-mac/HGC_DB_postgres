ad15748186f1b99c56492318ef478cf94ca440c6
Sindhu Murthy <58646122+murthysindhu@users.noreply.github.com>
2024-04-29 18:07:46 -0400

    Update localdb.py

--- localdb.py
+++ localdb.py
@@ -1,7 +1,28 @@
-import psycopg2
-import sshtunnel
-# import pwinput
-from psycopg2 import sql
+import asyncpg
+import yaml, os, subprocess
+
+def create_yaml_file():
+    data = {
+        'institution': f"{input('Set institution abbreviation: ')}",
+        'location': f"{input('Set institution location: ')}",
+    }
+    filename = 'dbase_info/institution_config.yaml'
+    if not os.path.exists(filename):
+        with open(filename, 'w') as file:
+            yaml.dump(data, file)
+            print(f"Created {filename} with default values.")
+          
+        with open('.gitignore', 'a') as gitignore:
+            gitignore.write(f"\n{filename}\n")
+            print(f"Added {filename} to .gitignore.")
+    else:
+        print(f"{filename} already exists. Skipping creation.")
+
+if __name__ == "__main__":
+    create_yaml_file()
+    subprocess.run(python, 'create/create_database.py')
+    subprocess.run(python, 'create/create_tables.py')
+    
 
 
 #######################
