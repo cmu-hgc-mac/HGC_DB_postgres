82b686918e849c496dcc730d0589286d0015dd5c
Sindhu Murthy <sindhumurthy97@gmail.com>
2024-12-03 20:17:19 -0500

    public access queries

--- mac_public/queries.py
+++ mac_public/queries.py
@@ -1,12 +1,14 @@
 import asyncpg, asyncio
 
-mod_simple_query = f"""SELECT 
+mod_simple_query = f"""SELECT DISTINCT ON (REPLACE(mpt.module_name,'-','')) 
     REPLACE(mpt.module_name,'-','') AS module_name, 
     mpt.status,
     mpt.status_desc,
     mpt.count_bad_cells,
     mpt.bias_vol,
-    mpt.trim_bias_voltage FROM module_pedestal_test AS mpt"""
+    mpt.trim_bias_voltage,
+    mpt.comment 
+    FROM module_pedestal_test AS mpt ORDER BY REPLACE(mpt.module_name,'-',''), mpt.mod_pedtest_no DESC"""
 
 mod_ped_query = f"""SELECT 
     REPLACE(mpt.module_name,'-','') AS module_name, 
@@ -24,24 +26,22 @@ mod_ped_query = f"""SELECT
     mi.sen_name,
     mi.bp_name
 FROM module_pedestal_test mpt JOIN module_info mi
-    ON REPLACE(mpt.module_name,'-','') = REPLACE(mi.module_name,'-','');
+    ON REPLACE(mpt.module_name,'-','') = REPLACE(mi.module_name,'-','') ;
 """
 
 hxb_ped_query = f"""SELECT 
-        REPLACE(hdp.hxb_name,'-','') as hxb_name, 
-        REPLACE(mi.module_name,'-','') AS module_name,
+        REPLACE(hdp.hxb_name, '-', '') AS hxb_name, 
+        REPLACE(mi.module_name, '-', '') AS module_name,
         hdp.status,
         hdp.status_desc,
-        hdp.bias_vol,
         hdp.trim_bias_voltage,
-        hdp.meas_leakage_current,
         hdp.count_bad_cells,
         hdp.list_dead_cells,
         hdp.list_noisy_cells,
-        hdp.list_disconnected_cells,
         hdp.comment
-    FROM module_pedestal_test hdp JOIN module_info mi
-    ON REPLACE(hdp.hxb_name,'-','') = REPLACE(mi.hxb_name,'-','');"""
+    FROM hxb_pedestal_test hdp 
+    LEFT JOIN module_info mi
+    ON REPLACE(hdp.hxb_name, '-', '') = REPLACE(mi.hxb_name, '-', '');"""
 
 mod_iv_query = mod_iv_query = f"""SELECT 
     REPLACE(miv.module_name,'-','') AS module_name, 
@@ -59,6 +59,5 @@ mod_iv_query = mod_iv_query = f"""SELECT
     mi.hxb_name, 
     mi.sen_name,
     mi.bp_name
-FROM mod_ivtest miv JOIN module_info mi
-    ON miv.module_name = mi.module_name;
-"""
+FROM module_iv_test miv JOIN module_info mi
+    ON miv.module_name = mi.module_name ; """
