a4d4fbe8fdbe7cd6b908284a2155b827e575971c
Sindhu Murthy <sindhumurthy97@gmail.com>
2024-12-18 21:26:49 -0600

    import parts from CMSR

--- import_data/get_parts_from_hgcapi.py
+++ import_data/get_parts_from_hgcapi.py
@@ -5,11 +5,14 @@ from cryptography.fernet import Fernet
 loc = 'dbase_info'
 conn_yaml_file = os.path.join(loc, 'conn.yaml')
 conn_info = yaml.safe_load(open(conn_yaml_file, 'r'))
+kop_yaml = yaml.safe_load(open(os.path.join('export', 'resource.yaml'), 'r')).get('kind_of_part')
 inst_code  = conn_info.get('institution_abbr')
+source_db_cern = conn_info.get('cern_db')
+db_source_dict = {'dev_db': {'dbname':'INT2R', 'url': 'hgcapi'} , 'prod_db': {'dbname':'CMSR', 'url': 'hgcapi-cmsr'}}
 
 db_params = {
     'database': conn_info.get('dbname'),
-    'user': 'ogp_user',
+    'user': 'editor',
     'host': conn_info.get('db_hostname'),
     'port': conn_info.get('port'),
 }
@@ -21,10 +24,17 @@ partTrans = {'bp' : {'apikey':'baseplates', 'dbtabname': 'bp_inspect', 'db_col':
              'ml' :{'apikey':'modules', 'dbtabname': 'module_inspect', 'db_col': 'module_name', 'qc_cols':  {'mod_grade': 'grade', 'mod_ave_thkns_mm': 'ave_thickness', "mod_max_thkns_mm": 'max_thickness', 'mod_fltns_mm': 'flatness', "pcb_plcment_x_offset": 'x_offset_mu', "pcb_plcment_y_offset": 'y_offset_mu',"pcb_plcment_ang_offset": 'ang_offset_deg'}},
             }
 
-def get_query_write(table_name, column_names):
-    pre_query = f""" INSERT INTO {table_name} ({', '.join(column_names)}) VALUES """
-    data_placeholder = ', '.join(['${}'.format(i) for i in range(1, len(column_names)+1)])
-    query = f"""{pre_query} {'({})'.format(data_placeholder)}"""
+partTransInit = {'bp': {'apikey':'baseplates', 'dbtabname': 'baseplate', 'db_cols': {'serial_number': 'bp_name', 'kind': 'kind', 'comment_description': 'comment'}},
+                'sen': {'apikey':'sensors',    'dbtabname': 'sensor',    'db_cols': {'serial_number': 'sen_name', 'kind': 'kind', 'comment_description': 'comment'}},
+                'hxb': {'apikey':'pcbs',       'dbtabname': 'hexaboard', 'db_cols': {'serial_number': 'hxb_name', 'kind': 'kind', 'comment_description': 'comment'}},
+            }
+
+def get_query_write(table_name, column_names, check_conflict_col = None, db_upload_data = None):
+    pre_query = f""" INSERT INTO {table_name} ({', '.join(column_names)}) SELECT """
+    data_placeholder = ', '.join([f'${i+1}' for i in range(len(column_names))])
+    query = f""" {pre_query} {f'{data_placeholder}'}  """
+    if check_conflict_col is not None:
+        query += f" WHERE NOT EXISTS ( SELECT 1 FROM {table_name} WHERE {check_conflict_col} = '{db_upload_data[check_conflict_col]}' ) "
     return query
 
 def check_exists_query(table_name, column_names):
@@ -33,33 +43,39 @@ def check_exists_query(table_name, column_names):
     query = f'{pre_query} {" AND ".join(data_placeholder)} );'
     return query
 
-async def write_to_db(pool, db_upload_data, partType = None):
-    table_name = partTrans[partType]["dbtabname"]
-    check_col = {key: db_upload_data[key] for key in [partTrans[partType]["db_col"], "date_inspect"]}
-    async with pool.acquire() as conn:
-        check_exists = await conn.fetchval( check_exists_query(table_name, check_col.keys()), *check_col.values())
-        if not check_exists:
-            query = get_query_write(table_name, db_upload_data.keys())
-            await conn.execute(query, *db_upload_data.values())
+# async def write_to_db(pool, db_upload_data, partType = None):
+#     table_name = partTrans[partType]["dbtabname"]
+#     check_col = {key: db_upload_data[key] for key in [partTrans[partType]["db_col"], "date_inspect"]}
+#     async with pool.acquire() as conn:
+#         check_exists = await conn.fetchval( check_exists_query(table_name, check_col.keys()), *check_col.values())
+#         if not check_exists:
+#             query = get_query_write(table_name, db_upload_data.keys())
+#             await conn.execute(query, *db_upload_data.values())
 
-def get_url(partID = None, macID = None, partType = None):
+async def write_to_db(pool, db_upload_data, partType = None, check_conflict_col=None):
+    table_name = partTransInit[partType]["dbtabname"]
+    async with pool.acquire() as conn:
+        query = get_query_write(table_name, db_upload_data.keys(), check_conflict_col=check_conflict_col, db_upload_data=db_upload_data)
+        await conn.execute(query, *db_upload_data.values())
+
+def get_url(partID = None, macID = None, partType = None, cern_db_url = 'hgcapi-cmsr'):
     if partID is not None:
-        return f'https://hgcapi.web.cern.ch/mac/part/{partID}/full'
+        return f'https://{cern_db_url}.web.cern.ch/mac/part/{partID}/full'
     elif partType is not None:
         if macID is not None:
-            return f'https://hgcapi.web.cern.ch/mac/parts/types/{partTrans[partType.lower()]["apikey"]}?page=0&limit=100&location={macID}'
-        return f'https://hgcapi.web.cern.ch/mac/parts/types/{partTrans[partType.lower()]["apikey"]}?page=0&limit=100'
+            return f'https://{cern_db_url}.web.cern.ch/mac/parts/types/{partTrans[partType.lower()]["apikey"]}?page=0&limit=100&location={macID}'
+        return f'https://{cern_db_url}.web.cern.ch/mac/parts/types/{partTrans[partType.lower()]["apikey"]}?page=0&limit=100'
     return
 
-def read_from_cern_db(partID = None, macID = None, partType = None ):
+def read_from_cern_db(partID = None, macID = None, partType = None , cern_db_url = 'hgcapi-cmsr'):
     headers = {'Accept': 'application/json'}
-    response = requests.get(get_url(partID = partID, macID = macID, partType = partType), headers=headers)
+    response = requests.get(get_url(partID = partID, macID = macID, partType = partType, cern_db_url = cern_db_url), headers=headers)
     if response.status_code == 200:
         data = response.json() ; 
 #         print(json.dumps(data, indent=2))
         return data
     else:
-        print(f'ERROR in reading from HGCAPI for partID : {partID} :: {response.status_code}')
+        print(f'ERROR in reading from {cern_db_url.upper()} for partID : {partID} :: {response.status_code}')
         return None
 
 def form(data):
@@ -69,6 +85,34 @@ def form(data):
         elif (data.lower() == 'none' or data.lower() == 'null' or data == ''):
             return None
     return data
+
+def get_part_type(partName, partType):
+    return_dict = {}
+    if partType == 'bp':
+        return_dict.update({'geometry' : kop_yaml['geometry'][partName[5]]})
+        return_dict.update({'resolution' : kop_yaml['resolution'][partName[6]]})
+        return_dict.update({'bp_material' : kop_yaml['material'][partName[7]]})
+    elif partType == 'hxb':
+        return_dict.update({'resolution': kop_yaml['resolution'][partName[4]]})  
+        return_dict.update({'geometry': kop_yaml['geometry'][partName[5]]})  
+        return_dict.update({'roc_version': kop_yaml['roc_version'][partName[7]]})  
+    elif partType == 'sen':
+        return_dict.update({'resolution': kop_yaml['sensor'][partName[0]][1]})  
+        return_dict.update({'geometry': kop_yaml['sensor_geometry'][partName[-1]]})  
+        return_dict.update({'thickness': int(kop_yaml['sensor'][partName[0]][0])})  
+    return return_dict
+
+def get_data_for_db_init(data_full, partType):
+    try:
+        db_dict = {partTransInit[partType]["db_cols"][k]: data_full[k] for k in (partTransInit[partType]["db_cols"]).keys()}
+        db_dict.update(get_part_type(data_full['serial_number'], partType))
+        return db_dict
+    except Exception as e:
+        traceback.print_exc()
+        print(f"ERROR in acquiring data from API output for {data_full['serial_number']}", e)
+        # print(json.dumps(data_full, indent=2))
+        # print('*'*100)
+        return None
 
 def get_data_for_db(data_full, partType):
     try:
@@ -125,23 +169,21 @@ async def main():
         dbpassword = cipher_suite.decrypt( base64.urlsafe_b64decode(args.password)).decode() ## Decode base64 to get encrypted string and then decrypt
         db_params.update({'password': dbpassword})
 
+    cern_db_url = db_source_dict[source_db_cern]['url']
     pool = await asyncpg.create_pool(**db_params)
-    for pt in ['bp','hxb','sen', 'pml', 'ml']:
-        print(f'Reading {partTrans[pt]["apikey"]} from HGCAPI ...' )
-        parts = (read_from_cern_db(macID = inst_code.upper(), partType = pt))['parts']
+    for pt in ['bp','hxb','sen']:  #, 'pml', 'ml']:
+        print(f'Reading {partTrans[pt]["apikey"]} from {cern_db_url.upper()} ...' )
+        parts = (read_from_cern_db(macID = inst_code.upper(), partType = pt, cern_db_url = cern_db_url))['parts']
         for p in parts:
             try:
-                data_full = read_from_cern_db(partID = p['serial_number'])
-                if data_full is not None:
-                    db_dict = get_data_for_db(data_full, partType = pt)
-                    if db_dict is not None:
-                        try:
-                            # print(db_dict)
-                            await write_to_db(pool, db_dict, partType = pt)
-                        except Exception as e:
-                            print(f'ERROR for single part upload for {data_full} {db_dict}', e)
-                            traceback.print_exc()
-                            print('Dictionary:', (db_dict))
+                db_dict = get_data_for_db_init(p, partType = pt)
+                if db_dict is not None:
+                    try:
+                        await write_to_db(pool, db_dict, partType = pt, check_conflict_col = partTransInit[pt]['db_cols']['serial_number'])
+                    except Exception as e:
+                        print(f"ERROR for single part upload for {p['serial_number']}", e)
+                        traceback.print_exc()
+                        print('Dictionary:', (db_dict))
             except:
                 traceback.print_exc()
         print(f'Writing {partTrans[pt]["apikey"]} to postgres complete.')
@@ -149,4 +191,24 @@ async def main():
     await pool.close()
     print('Refresh postgres tables')
 
+
 asyncio.run(main())
+
+    #             data_full = read_from_cern_db(partID = p['serial_number'], cern_db_url = cern_db_url)
+    #             if data_full is not None:
+    #                 db_dict = get_data_for_db(data_full, partType = pt)
+    #                 if db_dict is not None:
+    #                     try:
+    #                         # print(db_dict)
+    #                         await write_to_db(pool, db_dict, partType = pt)
+    #                     except Exception as e:
+    #                         print(f'ERROR for single part upload for {data_full} {db_dict}', e)
+    #                         traceback.print_exc()
+    #                         print('Dictionary:', (db_dict))
+    #         except:
+    #             traceback.print_exc()
+    #     print(f'Writing {partTrans[pt]["apikey"]} to postgres complete.')
+    #     print('-'*40); print('\n')
+    # await pool.close()
+    # print('Refresh postgres tables')
+
