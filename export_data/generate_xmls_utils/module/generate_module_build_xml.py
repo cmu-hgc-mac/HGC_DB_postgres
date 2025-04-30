d489cbfb50ad464aa6628623e5987a8f5862c70f
Manami-16 <manami.kanemura@gmail.com>
2025-04-30 11:06:45 -0400

    Formatted version as just 'V3c' or 'V3b'

--- export_data/generate_xmls_utils/module/generate_module_build_xml.py
+++ export_data/generate_xmls_utils/module/generate_module_build_xml.py
@@ -141,23 +141,26 @@ async def process_module(conn, yaml_file, xml_file_path, output_dir, date_start,
                         print('ERROR:', e)
 
                     if results:
-                        if xml_var == "RUN_BEGIN_TIMESTAMP_":
-                            # Fetching both ass_run_date and ass_time_begin
-                            run_date = results.get("ass_run_date", "")
-                            time_begin = results.get("ass_time_begin", "")
-                            if time_begin is None:
-                                time_begin = datetime.datetime.now().time()
-                            db_values[xml_var] = f"{run_date} {time_begin}"
-                        elif xml_var == "RUN_END_TIMESTAMP_":
-                            run_date = results.get("ass_run_date", "")
-                            time_end = results.get("ass_time_end", "")
-                            if time_end is None:
-                                time_end = datetime.datetime.now().time()
-                            db_values[xml_var] = f"{run_date} {time_end}"
-                        elif xml_var == 'PCB':
+                        # if xml_var == "RUN_BEGIN_TIMESTAMP_":
+                        #     # Fetching both ass_run_date and ass_time_begin
+                        #     run_date = results.get("ass_run_date", "")
+                        #     time_begin = results.get("ass_time_begin", "")
+                        #     if time_begin is None:
+                        #         time_begin = datetime.datetime.now().time()
+                        #     db_values[xml_var] = f"{run_date} {time_begin}"
+                        # elif xml_var == "RUN_END_TIMESTAMP_":
+                        #     run_date = results.get("ass_run_date", "")
+                        #     time_end = results.get("ass_time_end", "")
+                        #     if time_end is None:
+                        #         time_end = datetime.datetime.now().time()
+                        #     db_values[xml_var] = f"{run_date} {time_end}"
+                        if xml_var == 'PCB':
                             db_values[xml_var] = format_part_name(results.get('hxb_name'))
                         elif xml_var == 'PROTOMODULE':
                             db_values[xml_var] = format_part_name(results.get('proto_name'))
+                        elif xml_var == 'VERSION':
+                            roc_ver = results.get("roc_version", "")
+                            db_values[xml_var] = roc_ver[-3:]
                         else:
                             db_values[xml_var] = results.get(dbase_col, '') if not entry['nested_query'] else list(results.values())[0]
 
