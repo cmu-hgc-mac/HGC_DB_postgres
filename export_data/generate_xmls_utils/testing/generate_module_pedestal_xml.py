0b1c729f0a2bb414f67f6539a51ded5f29e9789e
Sindhu Murthy <sindhumurthy97@gmail.com>
2025-07-24 14:57:31 -0400

    include run_type

--- export_data/generate_xmls_utils/testing/generate_module_pedestal_xml.py
+++ export_data/generate_xmls_utils/testing/generate_module_pedestal_xml.py
@@ -162,6 +162,7 @@ async def generate_module_pedestal_xml(test_data, run_begin_timestamp, template_
     # === Fill in <RUN> metadata ===
     run_info = root.find("HEADER/RUN")
     if run_info is not None:
+        run_info.find("RUN_TYPE").text = "Si module pedestal and noise"
         run_info.find("RUN_NUMBER").text = get_run_num(LOCATION, test_timestamp)
         run_info.find("INITIATED_BY_USER").text = lxplus_username if lxplus_username is not None else "None"
         run_info.find("RUN_BEGIN_TIMESTAMP").text = format_datetime(run_begin_timestamp.split('T')[0], run_begin_timestamp.split('T')[1])
