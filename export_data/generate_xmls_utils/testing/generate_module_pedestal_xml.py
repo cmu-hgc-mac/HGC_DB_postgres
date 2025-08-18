a3e266fb5e08944a4a1dd8813306586dbe68e8e7
Manami-16 <manami.kanemura@gmail.com>
2025-08-18 13:48:09 -0400

    Added default vals of humidity and temp as 999

--- export_data/generate_xmls_utils/testing/generate_module_pedestal_xml.py
+++ export_data/generate_xmls_utils/testing/generate_module_pedestal_xml.py
@@ -144,8 +144,8 @@ async def fetch_test_data(conn, date_start, date_end, partsnamelist=None):
                 'module_name': row['module_name'],
                 'module_no': row['module_no'],
                 'inspector': row['inspector'],
-                'rel_hum': row['rel_hum'],
-                'temp_c': row['temp_c'],
+                'rel_hum': row['rel_hum'] if row['rel_hum'] is not None else 999,
+                'temp_c': row['temp_c'] if row['temp_c'] is not None else 999,
                 'roc_name': row['roc_name']
             }
     return test_data, test_data_env
