40fcf35c5cd15cd94ac732becbb14279fbc8382a
Sindhu Murthy <sindhumurthy97@gmail.com>
2025-10-10 12:23:03 -0400

    minor bug fix

--- export_data/generate_xmls_utils/testing/generate_hxb_pedestal_xml.py
+++ export_data/generate_xmls_utils/testing/generate_hxb_pedestal_xml.py
@@ -275,7 +275,7 @@ async def main(dbpassword, output_dir, date_start, date_end, encryption_key=None
                 float(test_data[run_begin_timestamp]['temp_c'])
                 output_file = await generate_hxb_pedestal_xml(test_data[run_begin_timestamp], run_begin_timestamp, output_dir, template_path_test=temp_dir, template_path_env = temp_dir_env, template_path_config=temp_dir_config, lxplus_username=lxplus_username)
             except:
-                print(f"{test_data['hxb_name']}: {run_begin_timestamp} You cannot upload any test data when humidity or temperature is null.") 
+                print(f"{test_data[run_begin_timestamp]['hxb_name']}: {run_begin_timestamp} You cannot upload any test data when humidity or temperature is null.") 
     except Exception as e:
         print(f"{RED}An error occurred: {e}. You cannot upload any test data when humidity or temperature is null.{RESET}")
     finally:
