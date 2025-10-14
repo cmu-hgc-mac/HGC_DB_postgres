6fc890ff5bbf6e80a4d518bd5bca86046452c5be
Sindhu Murthy <sindhumurthy97@gmail.com>
2025-10-14 19:16:19 -0400

    print by group

--- export_data/check_upload_xml_logs.py
+++ export_data/check_upload_xml_logs.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Tuple
 import re
 
-GREEN = "\033[32m"; RED = "\033[31m"; YELLOW = "\033[33m"; RESET = "\033[0m"; 
+GREEN = "\033[32m"; RED = "\033[31m"; YELLOW = "\033[33m"; RESET = "\033[0m"; MAGENTA = "\033[35m"
 remote_xml_dir = f"~/hgc_xml_temp"
 status_tracker = {'dbloader_failure': [], 'xml_issues': [] , 'dbloader_success' : []}
 
@@ -31,7 +31,7 @@ def analyze_log_status(log_path: str, upload_path: str, status_tracker = status_
     xmlfilename = Path(upload_path).name
     try:
         if not Path(log_path).exists():
-            message=(f"{RED}Log Missing: {xmlfilename}:{RESET} Log file not found")
+            message=(f"{MAGENTA}Log Missing: {xmlfilename}:{RESET} Log file not found")
             status_tracker['dbloader_failure'].append(message)
             return (f"Log Missing", "Log file not found")
 
@@ -39,7 +39,7 @@ def analyze_log_status(log_path: str, upload_path: str, status_tracker = status_
             lines = f.readlines()
 
         if not lines:
-            message=(f"{RED}Log Empty: {xmlfilename}:{RESET} Log file is empty")
+            message=(f"{MAGENTA}Log Empty: {xmlfilename}:{RESET} Log file is empty")
             status_tracker['dbloader_failure'].append(message)
             return ("Log Empty", "Log file is empty")
 
@@ -70,12 +70,12 @@ def analyze_log_status(log_path: str, upload_path: str, status_tracker = status_
             status_tracker['xml_issues'].append(message)
             return ("Missing/Wrong Variable", last_line)
 
-        message=(f"{RED}Error: {xmlfilename}: {RESET} {last_line}")
+        message=(f"{MAGENTA}Error: {xmlfilename}: {RESET} {last_line}")
         status_tracker['dbloader_failure'].append(message)
         return ("Error", last_line) # Default
 
     except Exception as e:
-        message=(f"{RED}Error Reading Log: {xmlfilename}{RESET}")
+        message=(f"{MAGENTA}Error Reading Log: {xmlfilename}{RESET}")
         status_tracker['dbloader_failure'].append(message)
         return (f"Error Reading Log: {e}", "")
     
@@ -92,7 +92,7 @@ def main():
     for log_path, upload_path in zip(log_paths, upload_paths):
         result = analyze_log_status(log_path, upload_path)
 
-    for k in status_tracker.keys():
+    for k in ['dbloader_success','dbloader_failure','xml_issues']:
         for m in status_tracker[k]:
             print(m)
 
